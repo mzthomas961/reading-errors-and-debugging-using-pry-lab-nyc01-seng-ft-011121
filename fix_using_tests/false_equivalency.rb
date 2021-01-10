caffc6be1df89aad723a79ca46af799a90036c85
Matthew Thomas <mzthomas96@gmail.com>
2021-01-10 19:20:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
     binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  if num = 2
+  if num == 2
   binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
